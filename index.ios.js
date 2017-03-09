0e9d529a2139dfb3d4cbe8820aec7248ecfd8f59
Morgan Miller <m.miller722@gmail.com>
2017-03-09 15:25:25 -0700

    moves FMTodo to its own component

--- index.ios.js
+++ index.ios.js
@@ -5,64 +5,9 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableHighlight
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { FMTodo } from './src/app/FMTodo';
 
-export default class FMTodo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [1,2,3],
-      newTodo: ''
-    }
-  }
-  render() {
-    console.log('STATE:')
-    console.log(this.state)
-    return (
-      <View style={styles.container}>
-        <TextInput style={{height: 60, backgroundColor: 'red'}} onChange={this.handleChange.bind(this)} value={this.state.newTodo}/>
-        <TouchableHighlight onPress={this.handlePress.bind(this)}>
-          <Text>tap me</Text>
-        </TouchableHighlight>
-        {this.state.todos.map((todo,i) => <Text key={i}>{todo}</Text>)}
-      </View>
-    );
-  }
-  handleChange(e) {
-    let value = e.nativeEvent.text;
-    this.setState({newTodo: value});
-  }
-  handlePress(e) {
-    let todos = this.state.todos;
-    todos.push(this.state.newTodo);
-    this.setState({todos: todos, newTodo: ''});
-  }
-}
+const Main = () => (<FMTodo />);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('FMTodo', () => FMTodo);
+AppRegistry.registerComponent('Main', () => Main);
