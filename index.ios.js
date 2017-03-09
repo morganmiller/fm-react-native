7e47727c8d39df313bba7726c7ca8a209cbaf07b
Morgan Miller <m.miller722@gmail.com>
2017-03-09 15:14:17 -0700

    gets add todo functionality working

--- index.ios.js
+++ index.ios.js
@@ -9,25 +9,40 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableHighlight
 } from 'react-native';
 
 export default class FMTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [1,2,3],
+      newTodo: ''
+    }
+  }
   render() {
+    console.log('STATE:')
+    console.log(this.state)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TextInput style={{height: 60, backgroundColor: 'red'}} onChange={this.handleChange.bind(this)} value={this.state.newTodo}/>
+        <TouchableHighlight onPress={this.handlePress.bind(this)}>
+          <Text>tap me</Text>
+        </TouchableHighlight>
+        {this.state.todos.map((todo,i) => <Text key={i}>{todo}</Text>)}
       </View>
     );
+  }
+  handleChange(e) {
+    let value = e.nativeEvent.text;
+    this.setState({newTodo: value});
+  }
+  handlePress(e) {
+    let todos = this.state.todos;
+    todos.push(this.state.newTodo);
+    this.setState({todos: todos, newTodo: ''});
   }
 }
 
